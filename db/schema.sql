5eec8e72032753b731e16a0d3018b6414b033c6f
Navneet Kaur <nbirdi@yahoo.com>
2022-08-02 11:24:10 +0800

    initial commit

--- db/schema.sql
+++ db/schema.sql
@@ -1,20 +1,20 @@
 DROP DATABASE IF EXISTS `employee_cms_demo` ;
 
 
-CREATE SCHEMA `employee_cms_demo` ;
+CREATE DATABASE `employee_cms_demo` ;
 
 
 use `employee_cms_demo` ;
 
 CREATE TABLE `departments` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`));
 
 
 
 CREATE TABLE `roles` (
-  `id` INT UNSIGNED NOT NULL,
+  `id` INT NOT NULL,
   `title` VARCHAR(255) NOT NULL,
   `salary` DECIMAL NOT NULL,
   `department_id` INT UNSIGNED NOT NULL,
@@ -22,19 +22,11 @@ CREATE TABLE `roles` (
 
 
 
-ALTER TABLE `roles` 
-ADD INDEX `fk_roles_1_idx` (`department_id` ASC) VISIBLE;
-;
-ALTER TABLE `employee_cms_demo`.`roles` 
-ADD CONSTRAINT `fk_roles_1`
-  FOREIGN KEY (`department_id`)
-  REFERENCES `employee_cms_demo`.`departments` (`id`)
-  ON DELETE CASCADE
-  ON UPDATE NO ACTION;
+
 
 
 CREATE TABLE `employee_cms_demo`.`employees` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(255) NOT NULL,
   `last_name` VARCHAR(255) NOT NULL,
   `role_id` INT UNSIGNED NOT NULL,
